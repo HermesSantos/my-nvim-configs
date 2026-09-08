4ec92ea44d08f127a3c376f16809f5fb1c29f7cf
HermesSantos <hermessantos995@gmail.com>
2026-09-08 10:49:07 -0300

    feat: add banger

---
+++ lua/plugins/banger.lua
@@ -0,0 +1,13 @@
+return {
+  "HermesSantos/banger",
+  keys = {
+    {
+      "!",
+      function()
+        require("banger").toggle()
+      end,
+      desc = "Toggle value under cursor",
+    },
+  },
+  opts = {},
+}
